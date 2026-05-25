43a3d5541e2e6ec9f643b49f49fb55b555d85fb5
241ai023 <z.shafan2006@gmail.com>
2026-05-25 17:12:47 +0530

    Add department and print  it

--- Practice.py
+++ Practice.py
@@ -2,5 +2,8 @@
 
 Name = input("Enter your name:").title()
 Age = int(input("Enter your age:"))
+Department = input("Enter your department:")
 
 print(f"My name is {Name} and i am {Age} years old.")
+print(f"My department name is {Department}")
+print("")
